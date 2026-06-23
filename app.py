2cdbb5c7e25583b65e1b8e51d666ee692abeae46
evgeniatrudova <68112616+evgeniatrudova@users.noreply.github.com>
2026-06-23 21:27:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+from plotly.subplots import make_subplots
 from fpdf import FPDF
 import io
 import hashlib
