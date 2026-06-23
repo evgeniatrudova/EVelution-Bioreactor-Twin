757e274ac7edd60aab5fe8405b451fa5dbfb22d1
evgeniatrudova <68112616+evgeniatrudova@users.noreply.github.com>
2026-06-23 21:03:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -384,7 +384,10 @@ with st.sidebar:
 model = FedBatchBioreactorModel()
 
 # RUN MASS BALANCE FIRST to get X(t)
-mb_df = model.run_mass_balance(vol, s_0, f_in, s_in, mu_max, o2, temp, ph, dur)
+mb_df = model.run_mass_balance(
+            vol, s_0, s_in, mu_max, o2, temp, ph, dur,
+            feed_strategy, f_in_initial, mu_setpoint, dilution_rate, is_chemostat
+        )
 final_biomass = mb_df["Biomass (g/L)"].iloc[-1]
 final_substrate = mb_df["Substrate (g/L)"].iloc[-1]
 final_vol = mb_df["Volume (L)"].iloc[-1]
